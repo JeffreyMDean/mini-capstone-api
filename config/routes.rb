aaee70e92a3f655d11985c2ec5e3d2ee310642c0
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-05-14 14:53:53 -0400

    added another route for first product

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/all_products" => "products#all"
+  get "/show_product" => "products#first"
 end
 
 
