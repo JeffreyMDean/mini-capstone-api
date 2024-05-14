2f39efb00b8f42b2a57dd24feda652dad4d90fe3
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-05-14 15:10:36 -0400

    added end to class

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
-end
+  def all
+    @products = Product.all
+    render template: "products/index"
+  end
+
+  def first
+    @product = Product.find_by(id: 1)
+    render template: "products/show"
+  end
+end 
