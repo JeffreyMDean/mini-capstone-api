e21bb7c08ac687c5b50f91ebc628bc2d7270e89b
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-06-18 15:55:51 -0400

    Added Category and CategoryProduct models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
+  products has_many :category_products 
   primary_abstract_class
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_07_173151) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_171737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_products", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "create_image1s", force: :cascade do |t|
     t.string "url"
@@ -63,6 +76,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_07_173151) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
   end
 
 end
