3037de89f55a4af65d08557c51cda6c73d01af85
JeffreyMDean <jeffrey.michael.dean@gmail.com>
2024-06-20 18:12:45 -0400

    modified products controller index action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
   before_action :authenticate_admin, except [:index, :show] #opposite would be only: [:create, :update, :destroy]
   def index
-    pp current_user
-    @products = Product.all 
+    if params[:name]
+      category = Category.find_by(name: params[:name])
+      @products = category.products
+    else
+      @products = Product.all
+    end
     render :index
   end
 
